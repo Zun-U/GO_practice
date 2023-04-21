6e7277eebbad0cba7c9b590e1f057fc9dfa0cf4d
Zun-U <hello.tra.uenishi@gmail.com>
2023-04-21 23:02:13 +0900

    methods

--- stady.go
+++ stady.go
@@ -19,6 +19,8 @@ import (
 
 	"goland/memory"
 
+
+	"goland/methods"
 )
 
 // 関数の外でvarによる変数宣言可能
@@ -45,6 +47,8 @@ func example() {
 
 	memory_test.Main()
 	memory_test.PointerTest()
+
+	learn_methods.MainMethod()
 }
 
 
